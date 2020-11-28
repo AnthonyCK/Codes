21ba7551dceb620d853f3bb1b988ed8937846176
Anthony Zhu <anthonyzhu268524@gmail.com>
2020-11-28 12:44:39 +0800

    Generate Transition Probability Matrix

--- MDP.py
+++ MDP.py
@@ -7,23 +7,39 @@ import math
 
 def gen_transitionProbMatrix(a, b, M, n, epsilon):
     """
-    docstring
+    Input
+    ----
+    a, b, M: 
+        Parameters for the linear regression;
+    n: 
+        Dimension of the transition probability matrix;
+    epsilon: 
+        Error of the regression.
+
+    Output
+    ----
+    TPM: 
+        Transition Probability Matrix.
     """
     demand = [i*math.floor(M/n) for i in range(n)]
     TPM = np.zeros((n, n))
     for i in range(n):
         mu = a * demand[i] * (M - demand[i]) + b
         for j in range(n):
-            if j < n-1:
+            if j == 0:
+                TPM[i][j] = scipy.stats.norm.cdf(demand[j+1], mu, epsilon)
+            elif j < n-1:
                 TPM[i][j] = scipy.stats.norm.cdf(demand[j+1], mu, epsilon) - scipy.stats.norm.cdf(demand[j], mu, epsilon)
             elif j == n-1:
-                TPM[i][j] = scipy.stats.norm.cdf(M, mu, epsilon) - scipy.stats.norm.cdf(demand[j], mu, epsilon)
+                TPM[i][j] = 1 - scipy.stats.norm.cdf(demand[j], mu, epsilon)
     return TPM
 
 
 
 def main():
-    tpm = gen_transitionProbMatrix(0.3, 2, 10, 5, 2)  
+    tpm = gen_transitionProbMatrix(0.25, 2, 10, 5, 2)  
+    for i in range(len(tpm)):
+        print(sum(tpm[i]))
     np.savetxt('TPM', tpm)  
 
 if __name__ == '__main__':
